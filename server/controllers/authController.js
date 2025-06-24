77b16e2f854602faa43a9f52abbc4dd2a798d86c
prathamsaxena1 <pratham.s@turing.com>
2025-06-25 00:30:01 +0530

    interaction 3 Conversation 2_A

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,307 +1,113 @@
 // controllers/authController.js
-const crypto = require('crypto');
-const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('../utils/asyncHandler');
-const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
-// @desc    Register user
-// @route   POST /api/v1/auth/register
-// @access  Public
-exports.register = asyncHandler(async (req, res, next) => {
-  const { name, email, password, role, phone } = req.body;
-
-  // Create user
-  const user = await User.create({
-    name,
-    email,
-    password,
-    role: role || 'customer', // Default to customer if role not specified
-    phone
-  });
-
-  // Generate verification token
-  if (!user.isVerified) {
-    const verificationToken = user.getVerificationToken();
-    await user.save({ validateBeforeSave: false });
-
-    // Create verification url
-    const verificationUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/verify-email/${verificationToken}`;
-
-    const message = `You are receiving this email because you need to confirm your email address. Please make a GET request to: \n\n ${verificationUrl}`;
-
+// Register a new user
+exports.registerUser = async (req, res) => {
     try {
-      await sendEmail({
-        email: user.email,
-        subject: 'Email Verification Token',
-        message
-      });
+        const { name, email, password } = req.body;
 
-      sendTokenResponse(user, 200, res);
-    } catch (err) {
-      user.verificationToken = undefined;
-      await user.save({ validateBeforeSave: false });
+        // Create user
+        const user = await User.create({
+            name,
+            email,
+            password // This will be automatically hashed by our pre-save middleware
+        });
 
-      return next(new ErrorResponse('Email could not be sent', 500));
+        // Remove password from response
+        user.password = undefined;
+
+        res.status(201).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        });
     }
-  } else {
-    sendTokenResponse(user, 200, res);
-  }
-});
+};
 
-// @desc    Verify email
-// @route   GET /api/v1/auth/verify-email/:verificationToken
-// @access  Public
-exports.verifyEmail = asyncHandler(async (req, res, next) => {
-  // Get hashed token
-  const verificationToken = crypto
-    .createHash('sha256')
-    .update(req.params.verificationToken)
-    .digest('hex');
+// controllers/authController.js
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-  const user = await User.findOne({
-    verificationToken,
-  });
+        // Validate email & password
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: 'Please provide email and password'
+            });
+        }
 
-  if (!user) {
-    return next(new ErrorResponse('Invalid token', 400));
-  }
+        // Find user by email and include password field
+        const user = await User.findOne({ email }).select('+password');
 
-  // Set isVerified to true and remove verificationToken
-  user.isVerified = true;
-  user.verificationToken = undefined;
-  await user.save();
+        // Check if user exists
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid credentials'
+            });
+        }
 
-  sendTokenResponse(user, 200, res);
-});
+        // Check if password matches
+        const isMatch = await user.matchPassword(password);
 
-// @desc    Login user
-// @route   POST /api/v1/auth/login
-// @access  Public
-exports.login = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid credentials'
+            });
+        }
 
-  // Validate email & password
-  if (!email || !password) {
-    return next(new ErrorResponse('Please provide an email and password', 400));
-  }
+        // Password is correct, send response (you might generate a JWT here)
+        // Remove password from response
+        user.password = undefined;
 
-  // Check for user
-  const user = await User.findOne({ email }).select('+password');
-
-  if (!user) {
-    return next(new ErrorResponse('Invalid credentials', 401));
-  }
-
-  // Check if password matches
-  const isMatch = await user.matchPassword(password);
-
-  if (!isMatch) {
-    return next(new ErrorResponse('Invalid credentials', 401));
-  }
-
-  sendTokenResponse(user, 200, res);
-});
-
-// @desc    Log user out / clear cookie
-// @route   GET /api/v1/auth/logout
-// @access  Private
-exports.logout = asyncHandler(async (req, res, next) => {
-  res.cookie('token', 'none', {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
-  });
-
-  res.status(200).json({
-    success: true,
-    data: {}
-  });
-});
-
-// @desc    Get current logged in user
-// @route   GET /api/v1/auth/me
-// @access  Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id).populate({
-    path: 'restaurants',
-    select: 'name location.city cuisine rating'
-  });
-
-  res.status(200).json({
-    success: true,
-    data: user
-  });
-});
-
-// @desc    Update user details
-// @route   PUT /api/v1/auth/updatedetails
-// @access  Private
-exports.updateDetails = asyncHandler(async (req, res, next) => {
-  const fieldsToUpdate = {
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    address: req.body.address
-  };
-
-  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
-    new: true,
-    runValidators: true
-  });
-
-  res.status(200).json({
-    success: true,
-    data: user
-  });
-});
-
-// @desc    Update password
-// @route   PUT /api/v1/auth/updatepassword
-// @access  Private
-exports.updatePassword = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id).select('+password');
-
-  // Check current password
-  if (!(await user.matchPassword(req.body.currentPassword))) {
-    return next(new ErrorResponse('Password is incorrect', 401));
-  }
-
-  user.password = req.body.newPassword;
-  await user.save();
-
-  sendTokenResponse(user, 200, res);
-});
-
-// @desc    Forgot password
-// @route   POST /api/v1/auth/forgotpassword
-// @access  Public
-exports.forgotPassword = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({ email: req.body.email });
-
-  if (!user) {
-    return next(new ErrorResponse('There is no user with that email', 404));
-  }
-
-  // Get reset token
-  const resetToken = user.getResetPasswordToken();
-
-  await user.save({ validateBeforeSave: false });
-
-  // Create reset url
-  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
-
-  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
-
-  try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Password reset token',
-      message
-    });
-
-    res.status(200).json({ success: true, data: 'Email sent' });
-  } catch (err) {
-    console.log(err);
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-
-    await user.save({ validateBeforeSave: false });
-
-    return next(new ErrorResponse('Email could not be sent', 500));
-  }
-});
-
-// @desc    Reset password
-// @route   PUT /api/v1/auth/resetpassword/:resettoken
-// @access  Public
-exports.resetPassword = asyncHandler(async (req, res, next) => {
-  // Get hashed token
-  const resetPasswordToken = crypto
-    .createHash('sha256')
-    .update(req.params.resettoken)
-    .digest('hex');
-
-  const user = await User.findOne({
-    resetPasswordToken,
-    resetPasswordExpire: { $gt: Date.now() }
-  });
-
-  if (!user) {
-    return next(new ErrorResponse('Invalid token', 400));
-  }
-
-  // Set new password
-  user.password = req.body.password;
-  user.resetPasswordToken = undefined;
-  user.resetPasswordExpire = undefined;
-  await user.save();
-
-  sendTokenResponse(user, 200, res);
-});
-
-// @desc    Create new user (admin only)
-// @route   POST /api/v1/auth/users
-// @access  Private/Admin
-exports.createUser = asyncHandler(async (req, res, next) => {
-  const user = await User.create(req.body);
-
-  res.status(201).json({
-    success: true,
-    data: user
-  });
-});
-
-// @desc    Update user role (admin only)
-// @route   PUT /api/v1/auth/users/:id/role
-// @access  Private/Admin
-exports.updateUserRole = asyncHandler(async (req, res, next) => {
-  const user = await User.findByIdAndUpdate(
-    req.params.id,
-    { role: req.body.role },
-    {
-      new: true,
-      runValidators: true
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
-  );
+};
 
-  if (!user) {
-    return next(new ErrorResponse(`No user with the id of ${req.params.id}`, 404));
-  }
+// controllers/authController.js
+exports.updatePassword = async (req, res) => {
+    try {
+        const { currentPassword, newPassword } = req.body;
 
-  res.status(200).json({
-    success: true,
-    data: user
-  });
-});
+        // Get user with password
+        const user = await User.findById(req.user.id).select('+password');
 
-// Get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-  // Create token
-  const token = user.getSignedJwtToken();
+        // Check current password
+        const isMatch = await user.matchPassword(currentPassword);
 
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true
-  };
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: 'Current password is incorrect'
+            });
+        }
 
-  if (process.env.NODE_ENV === 'production') {
-    options.secure = true;
-  }
+        // Update password
+        user.password = newPassword; // This will trigger our pre-save middleware
+        await user.save();
 
-  res
-    .status(statusCode)
-    .cookie('token', token, options)
-    .json({
-      success: true,
-      token,
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-        isVerified: user.isVerified
-      }
-    });
+        res.status(200).json({
+            success: true,
+            message: 'Password updated successfully'
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
 };
--- server/models/User.js
+++ server/models/User.js
@@ -136,4 +136,29 @@ UserSchema.virtual('fullName').get(function() {
 // Add index for better search performance
 UserSchema.index({ email: 1 });
 
-module.exports = mongoose.model('User', UserSchema);
+
+// Pre-save middleware to hash password
+UserSchema.pre('save', async function(next) {
+    // Only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) {
+      return next();
+    }
+    
+    try {
+      // Generate a salt with cost factor 10
+      const salt = await bcrypt.genSalt(10);
+      
+      // Hash the password using the generated salt
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  // Method to compare entered password with hashed password
+  UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+  
+  module.exports = mongoose.model('User', UserSchema);
